e2f7afee2fe1f60e165c835bb84a8fd4856fc88e
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-10 09:27:16 +0000

    Added test for not displaying other users posts

--- frontend/cypress/e2e/viewing_profile_page.cy.js
+++ frontend/cypress/e2e/viewing_profile_page.cy.js
@@ -1,17 +1,30 @@
-describe('Visiting account page', () => {
+describe("Visiting account page", () => {
   before(() => {
-    cy.signup('user@email.com', '12345678', 'orhan');
-    cy.login('user@email.com', '12345678');
+    cy.signup("hello@gmail.com", "123");
   });
 
-  it('Can visit account page', () => {
-    cy.visit('/profile');
-    cy.get('h2').contains('Profile');
+  it("Can visit account page", () => {
+    cy.login("hello@gmail.com", "123");
+    cy.visit("/profile");
+    cy.get("h2").contains("Profile");
   });
 
-  // it('returns post of user', () => {
-  //   cy.makePost('Makers are the best');
-  //   cy.visit('/profile');
-  //   // cy.get('[data-cy="post"]').should('contain.text', 'Makers are the best');
-  // });
+  it("Renders posts of the user", () => {
+    cy.login("hello@gmail.com", "123");
+    cy.makePost("Makers are the best");
+    cy.visit("/profile");
+    cy.get('[data-cy="post"]').should("contain.text", "Makers are the best");
+  });
+
+  it("Should not render posts from other users", () => {
+    cy.signup("anotheruser@yahoo.co.uk", "password");
+    cy.login("anotheruser@yahoo.co.uk", "password");
+    cy.makePost("What a great website!");
+    cy.login("hello@gmail.com", "123");
+    cy.visit("/profile");
+    cy.get('[data-cy="post"]').should(
+      "not.contain.text",
+      "What a great website!"
+    );
+  });
 });
