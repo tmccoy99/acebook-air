9dc4ea81db96dd8a31608ebea9ea2526a5f16816
terrybro66 <terrybro61@gmail.com>
2023-02-14 11:38:36 +0000

    fixing home page image

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import CreatePostForm from '../createPostForm/createPostForm';
-import Post from '../post/Post';
+import React, { useEffect, useState } from "react";
+import CreatePostForm from "../createPostForm/createPostForm";
+import Post from "../post/Post";
 
 const Feed = ({ reload, setReload, navigate, path }) => {
   const [posts, setPosts] = useState([]);
-  const [token, setToken] = useState(window.localStorage.getItem('token'));
-  const [id, setId] = useState(window.localStorage.getItem('user_id'));
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [id, setId] = useState(window.localStorage.getItem("user_id"));
 
   useEffect(() => {
     if (token) {
-      fetch(path || '/posts', {
+      fetch(path || "/posts", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
         .then((response) => response.json())
         .then(async (data) => {
-          window.localStorage.setItem('token', data.token);
-          setToken(window.localStorage.getItem('token'));
-          setId(window.localStorage.getItem('user_id'));
+          window.localStorage.setItem("token", data.token);
+          setToken(window.localStorage.getItem("token"));
+          setId(window.localStorage.getItem("user_id"));
           setPosts(data.posts);
           setReload(false);
         });
     } else {
-      navigate('/login');
+      navigate("/");
     }
   }, [reload]);
 
   const logout = () => {
-    window.localStorage.removeItem('token');
-    navigate('/login');
+    window.localStorage.removeItem("token");
+    navigate("/login");
   };
 
   if (token) {
@@ -38,7 +38,7 @@ const Feed = ({ reload, setReload, navigate, path }) => {
         <h2>Posts</h2>
         <button onClick={logout}>Logout</button>
 
-        <div id='feed' role='feed'>
+        <div id="feed" role="feed">
           {posts ? (
             posts
               .slice(0)
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./Home.css";
+import styles from "./Home.module.css";
 
 const Home = ({ navigate }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Welcome to Acebook</h2>
-        <p>Acebook is a social media platform that allows users to connect with friends, family, organizations and public figures. As part of the community you can interact to other users and share pictures and posts. </p>
+      <p>
+        Acebook is a social media platform that allows users to connect with
+        friends, family, organizations and public figures. As part of the
+        community you can interact to other users and share pictures and posts.{" "}
+      </p>
+      <div className={styles.links}>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
         <p></p>
-        <ul className={styles.links}>
-          <Link to="/login"><button>Login</button></Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
         <p></p>
-          <Link to="/signup"><button>Sign Up</button></Link>
-        <p></p>
-        </ul>
-      <div>
+      </div>
+      <div className={styles.homeImage}>
         <img src="/images/noback-community-acebook.svg" alt="home" />
       </div>
     </div>
   );
-}
+};
 
 export default Home;
