7a0cfea7a89a3c0bd760b55cf472686610bc8377
Joe Burgess <josephburgess@gmail.com>
2023-02-16 16:48:57 +0000

    dotenv

--- frontend/src/components/account/form/form.js
+++ frontend/src/components/account/form/form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-require('dotenv').config();
+require('dotenv-safe').config();
 const Form = ({ form_type, token, setShowForm }) => {
   const [inputValue, setInputValue] = useState('');
   const [file, setFile] = useState(null);
