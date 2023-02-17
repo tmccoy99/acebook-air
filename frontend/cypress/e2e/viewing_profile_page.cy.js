f4c2e4c6123800d4da6dbb9fb137bcb0246abe7a
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-02-17 10:49:29 +0000

    Updating welcome message

--- frontend/cypress/e2e/viewing_profile_page.cy.js
+++ frontend/cypress/e2e/viewing_profile_page.cy.js
@@ -6,6 +6,6 @@ describe('Visiting profile page', () => {
   it('Can visit account page', () => {
     cy.login('user@email.com', '12345678');
     cy.visit('/profile');
-    cy.get('h2').contains('Profile');
+    cy.get('h2').contains('Welcome to your Profile page');
   });
 });
